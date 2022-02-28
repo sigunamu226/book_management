ff6bad09c4920b6c568ae9b6bd3f0749cd848b75
sigunamu226 <sigunamu226@gmail.com>
2022-02-28 22:55:53 +0900

    一覧の表示制限を無くす

--- app/Http/Controllers/BooksController.php
+++ app/Http/Controllers/BooksController.php
@@ -17,7 +17,7 @@ class BooksController extends Controller
 
     //本一覧表示
     public function index(){
-        $books = Book::where('user_id',Auth::user()->id)->orderBy('created_at', 'asc')->paginate(5);
+        $books = Book::where('user_id',Auth::user()->id)->orderBy('created_at', 'asc');
         return view('books', [
             'books' => $books
         ]);
